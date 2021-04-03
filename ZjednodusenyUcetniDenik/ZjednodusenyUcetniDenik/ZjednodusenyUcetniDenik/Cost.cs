7c18d6f23e917e36a4d6efd7492e79634887dcd2
Lenka Ruffi <lenkaruffi@gmail.com>
2021-04-03 11:30:00 +0200

    Základ pro projekt

---
+++ ZjednodusenyUcetniDenik/ZjednodusenyUcetniDenik/ZjednodusenyUcetniDenik/Cost.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZjednodusenyUcetniDenik
+{
+    class Cost : Item
+    {
+        public double CostAmount { get; private set; }
+    }
+}
---
+++ ZjednodusenyUcetniDenik/ZjednodusenyUcetniDenik/ZjednodusenyUcetniDenik/Item.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZjednodusenyUcetniDenik
+{
+    class Item
+    {
+        public string InvoiceNumber { get; private set; }
+        public string InvoiceDescription { get; private set; }
+        public string CounterpartyName { get; private set; }
+        public string CounterpartyAddressStreet { get; private set; }
+        public string CounterpartyAddressZipCode { get; private set; }
+        public string CounterpartyAddressTown { get; private set; }
+        public string CounterpartyIdentificateNumber { get; private set; } //IČ
+        public string CounterpartyTaxIdentityNumber { get; private set; } //DIČ
+        public DateTime InvoiceDate { get; private set; } //DUZP
+        public DateTime DueDate { get; private set; } //Datum splatnosti
+        public DateTime PaymentDate { get; private set; } //Datum úhrady
+        public string ItemCategory { get; private set; }
+
+    }
+}
