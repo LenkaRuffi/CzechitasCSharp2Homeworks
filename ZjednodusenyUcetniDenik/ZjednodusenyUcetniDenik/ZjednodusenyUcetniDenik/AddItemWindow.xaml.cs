373817bdcc4a3f87c17970bdfd20ec95facf2828
Lenka Ruffi <lenkaruffi@gmail.com>
2021-04-05 14:03:36 +0200

    pridano okno add item

--- ZjednodusenyUcetniDenik/ZjednodusenyUcetniDenik/ZjednodusenyUcetniDenik/AddItemWindow.xaml.cs
+++ ZjednodusenyUcetniDenik/ZjednodusenyUcetniDenik/ZjednodusenyUcetniDenik/AddItemWindow.xaml.cs
@@ -10,30 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace ZjednodusenyUcetniDenik
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for AddItemWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AddItemWindow : Window
     {
-        public MainWindow()
+        public AddItemWindow()
         {
             InitializeComponent();
-        }
-
-        private void buttonFiltering_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void AddItem_Click(object sender, RoutedEventArgs e)
-        {
-            AddItemWindow addItemWindow = new AddItemWindow();
-            addItemWindow.ShowDialog();
         }
     }
 }
