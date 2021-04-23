a19b6ea72e73b8b3b1558da5998a91a45c639693
Lenka Ruffi <lenkaruffi@gmail.com>
2021-04-23 14:49:20 +0200

    Projekt: oprava chyby ikonek

--- ZjednodusenyUcetniDenik/ZjednodusenyUcetniDenik/ZjednodusenyUcetniDenik/Properties/Resources.Designer.cs
+++ ZjednodusenyUcetniDenik/ZjednodusenyUcetniDenik/ZjednodusenyUcetniDenik/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace ZjednodusenyUcetniDenik.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap filter_icon {
-            get {
-                object obj = ResourceManager.GetObject("filter_icon", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
