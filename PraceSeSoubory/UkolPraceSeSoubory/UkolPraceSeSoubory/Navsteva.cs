d56d991a91eb95fa22e70b895986fe99aff346af
Lenka Ruffi <lenkaruffi@gmail.com>
2021-04-25 10:12:36 +0200

    Ukol: Vytvoreni noveho ukolu prace se soubory

---
+++ PraceSeSoubory/UkolPraceSeSoubory/UkolPraceSeSoubory/Navsteva.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UkolPraceSeSoubory
+{
+    class Navsteva
+    {
+        public string Jmeno { get; set; }
+
+        public int Vek { get; set; }
+
+        public Navsteva()
+        {
+
+        }
+
+        public Navsteva(int vek, string jmeno)
+        {
+            Jmeno = jmeno;
+            Vek = vek;
+        }
+
+    }
+}
---
+++ PraceSeSoubory/UkolPraceSeSoubory/UkolPraceSeSoubory/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace UkolPraceSeSoubory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Do proměnné typu string uložte cestu na plochu aktuálního uživatele
+Zkontrolujte, že na ploše existuje složka Czechitas, a pokud ne, vytvořte ji
+Vytvořte nebo použijte třídu Navsteva (z lekce) a vytvořte list s alespoň 2 návštěvami
+Do složky Czechitas uložte soubor navstevy.csv, ve kterém bude na každém řádku uložena informace o jedné návštěvě z listu, oddělená čárkou např:
+Jarda,10,
+Vitek,11,
+Potom z CSV souboru načtěte návštěvy a zobrazte na konzoli
+Pokud soubor csv již existuje, při spuštění programu jej přepište.
+Nepoužívejte pomocné knihovny pro práci s CSV (CsvHelper apod), csv je jednoduchý formát, který načte třeba Excel :) */
+
+            string cestaNaPlochuAktUzivalele = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        }
+    }
+}
